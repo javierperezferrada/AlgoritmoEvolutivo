c96bad096bc2e58a713664ade5dde64e54dd4cdf
Melombus <tvargas.vicencio@gmail.com>
2016-12-04 11:58:34 -0300

    Fix alg-evolutivo + simulacion + generacion

--- AlgoritmoEvolutivo.py
+++ AlgoritmoEvolutivo.py
@@ -2,6 +2,7 @@ import random
 import uuid
 from pyelasticsearch import ElasticSearch
 import os
+import time
 
 es = ElasticSearch('http://localhost:9202/')
 
@@ -51,7 +52,7 @@ def generatePoblation(n,universe):
     #save individuals in ElasticSearch
     rangeRandom = len(universe)
     for i in range(n):
-        poblation = {'adn':[],'simulated':'not'}
+        poblation = {'adn':[],'simulated':False}
         for j in range(5):
             poblation['adn'].append(universe[random.randint(0, rangeRandom-1)])
         try:
@@ -60,27 +61,33 @@ def generatePoblation(n,universe):
                                 poblation)
         except Exception as e:
             print e
+        print "individuo %s creado" % i
 
 def simulate():
+    print "Esperando"
+    time.sleep(5)
     #simulate all individuals in poblation pobl
     #agregate time for each individual in pobl
     #query = {'sort':[{'art_date':{'order':'asc'}}],'query':{'match_all':{}},}
     query = {'query':{'bool':{
                 'filter':[
-                {'term':{'simulated':'not'}}
+                {'term':{'simulated':False}}
                 ]
     }},}
     try:
         #request all articles to ElasticSearch
         totalInd = es.count(query,index='ia')
-        allIndividuals = es.search(query,size=totalInd['count'], index='ia')
+        allIndividuals = es.search(query, size=totalInd['count'], index='ia')
+        print "no simulados: "+str(totalInd['count'])
     except Exception as e:
         #if fail conection to ElasticSearch
         print e
     #print allIndividuals
     data = allIndividuals['hits']['hits']
-
+    #print data
+    i=0
     for key in data:
+        print i
         #print key
         #print len(key['_source']['adn'])
         #CREAR ARCHIVO Y METER key['_source']['adn'][cesar]
@@ -89,17 +96,21 @@ def simulate():
             #print key['_source']['adn'][cesar]
             combi_puertas.write(key['_source']['adn'][cesar]+" \n")
             #combi_puertas.write("\n")
+        combi_puertas.close()
+        time.sleep(1)
         os.system("java -Xmx1024m -Dfile.encoding=UTF-8 -cp NetLogo.jar org.nlogo.headless.Main --model escape4.nlogo --experiment simulation")
         segundos = open("seconds.output", "r")
         for linea in segundos:
             linea = linea.rstrip("\n")
             print "segundos: %s" % (linea)
-            queryDoc = {'time':int(linea),'simulated':'yes'}
+            queryDoc = {'time':int(linea),'simulated':True}
 
         try:
             es.update('ia','individual',key['_id'],doc=queryDoc)
         except Exception as e:
             print e
+        i+=1
+
         #EN VEZ DE IMPRIMIR, CREAR ARCHIVO 
         #
         #
@@ -121,13 +132,16 @@ def selection(nPar):
     try:
         #request all articles to ElasticSearch
         totalInd = es.count(query,index='ia')
+        print "total:"+str(totalInd)
         bestIndividuals = es.search(query,size=nPar, index='ia')
     except Exception as e:
         #if fail conection to ElasticSearch
         print e
     #print bestIndividuals
     data = bestIndividuals['hits']['hits']
-    for i in range(nPar/2):
+    i=0
+    while i<nPar:
+        print i
         ind1 = bestIndividuals['hits']['hits'][i]['_source']['adn']
         ind2 = bestIndividuals['hits']['hits'][i+1]['_source']['adn']
         #print ind1
@@ -139,13 +153,35 @@ def selection(nPar):
                 new.append(ind1[j])
             else:
                 new.append(ind2[j])
-        newIndividual = {'adn':new}
+        newIndividual = {'adn':new,'simulated':False}
         try:
             responseElastic = es.index('ia',
                                 'individual',
                                 newIndividual)
+            print "hijo agregado"
         except Exception as e:
             print e
+        i+=2
+    '''for i in range(nPar/2):
+        ind1 = bestIndividuals['hits']['hits'][i]['_source']['adn']
+        ind2 = bestIndividuals['hits']['hits'][i+1]['_source']['adn']
+        #print ind1
+        #print ind2
+        new = []
+        for j in range(5):
+            rand = random.randint(0,10)
+            if rand<=5:
+                new.append(ind1[j])
+            else:
+                new.append(ind2[j])
+        newIndividual = {'adn':new,'simulated':'not'}
+        try:
+            responseElastic = es.index('ia',
+                                'individual',
+                                newIndividual)
+            print "hijo agregado"
+        except Exception as e:
+            print e'''
     simulate()
 
 def pair(pobl):
@@ -162,22 +198,24 @@ def selectBest(pobl,npar):
 
 def summary():
     #evaluate result of simulate
+    total = {'query':{'match_all':{}},}
     query = {'aggs': { 'avg_time' : {'avg': { 'field': 'time'}}}}
     query2 = {'aggs': { 'min_time' : {'min': { 'field': 'time'}}}}
     try:
         #request all articles to ElasticSearch
-        totalInd = es.count(query,index='ia')
-        #print totalInd
+        totalInd = es.count(total,index='ia')
         average = es.search(query, index='ia')
         minimum = es.search(query2, index='ia')
         print average['aggregations']['avg_time']['value']
         print minimum['aggregations']['min_time']['value']
+        f = open("grafico.csv", "a")
+        f.write(str(totalInd['count'])+","+str(average['aggregations']['avg_time']['value'])+","+str(minimum['aggregations']['min_time']['value'])+"\n")
     except Exception as e:
         #if fail conection to ElasticSearch
         print e
-    newPoblationResult = {'minimum':minimum['aggregations']['min_time']['value'],'average':average['aggregations']['avg_time']['value']}
+    newPoblationResult = {'minimum':minimum['aggregations']['min_time']['value'],'average':average['aggregations']['avg_time']['value'],'poblation':totalInd['count']}
     try:
-        es.index('ia',
+        es.index('results',
                 'results',
                 newPoblationResult)
     except Exception as e:
@@ -186,28 +224,36 @@ def summary():
 
 #main AlgoritmoEvolutivo
 class AlgoritmoEvolutivo():
-    #all points where can door ubicated
-    #allCanDoor = readPlan('plan.plan')
-    #generate n poblations of combinated five random points with doors
-    #generatePoblation(20, allCanDoor)
-    #evaluate all individuals in poblation
-    #simulate()
-    #evaluate results
-    #selection(10)
-    #selectBest(poblation,10)
-    #pair poblation
-    #childrens = pair(poblation)
-    #print poblation
-    #summary()
-    query = {'query':{'match_all':{}},}
-    try:
-        #request all articles to ElasticSearch
-        totalInd = es.count(query,index='ia')
-        allIndividuals = es.search(query,size=totalInd['count'], index='ia')
-    except Exception as e:
-        #if fail conection to ElasticSearch
-        print e
-    print allIndividuals
+    i=0
+    while i<10:
+        #all points where can door ubicated
+        allCanDoor = readPlan('plan.plan')
+        #generate n poblations of combinated five random points with doors
+        generatePoblation(20, allCanDoor)
+        #evaluate all individuals in poblation
+        simulate()
+        #evaluate results
+        selection(10)
+        #selectBest(poblation,10)
+        #pair poblation
+        #childrens = pair(poblation)
+        #print poblation
+        summary()
+        query = {'query':{'bool':{
+                    'filter':[
+                    {'term':{'simulated':False}}
+                    ]
+        }},}
+        try:
+            #request all articles to ElasticSearch
+            totalInd = es.count(query,index='ia')
+            allIndividuals = es.search(query,size=totalInd['count'], index='ia')
+        except Exception as e:
+            #if fail conection to ElasticSearch
+            print e
+        print totalInd
+        #print allIndividuals
+        i+=1
 
 
 
